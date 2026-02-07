d7a77d5affe6fc27ca9edd2ff1f4f5d7100e2ca4
paigely <235818692+paigely@users.noreply.github.com>
2026-02-07 03:19:27 +0100

    fix: lyrics screen padding

--- composeApp/src/commonMain/kotlin/paige/navic/ui/screen/LyricsScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screen/LyricsScreen.kt
@@ -17,9 +17,14 @@ import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.plus
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -176,7 +181,12 @@ fun LyricsScreen(
 						}
 					}
 
-					LazyColumn(Modifier.fillMaxSize(), state = listState) {
+					LazyColumn(
+						Modifier.fillMaxSize(),
+						state = listState,
+						contentPadding = WindowInsets.statusBars.asPaddingValues()
+							+ WindowInsets.systemBars.asPaddingValues()
+					) {
 						itemsIndexed(lyrics) { index, (startTime, text) ->
 							val isActive = index == activeIndex
 
