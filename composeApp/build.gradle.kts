cc9e34f25a81263c5af588dd86f6689a018f5eff
paige <235818692+paigely@users.noreply.github.com>
2026-02-25 12:33:56 +0100

    chore: update altstore repo

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -193,6 +193,7 @@ android {
 
 	applicationVariants.all {
 		outputs.all {
+			val output = this as com.android.build.gradle.internal.api.ApkVariantOutputImpl
 			output.outputFileName = "Navic.apk"
 		}
 	}
