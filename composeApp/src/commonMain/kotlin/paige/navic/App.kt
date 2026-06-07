2c5d2dcfceb5c42b1dc57ec96a982d0f700280ce
Heropowwa <fittiziogiovanni@gmail.com>
2026-06-07 21:51:13 +0200

    fix: use Viewmodel in App

--- composeApp/src/commonMain/kotlin/paige/navic/App.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/App.kt
@@ -63,6 +63,7 @@ import paige.navic.shared.MediaPlayerViewModel
 import paige.navic.ui.components.snackbars.NavicSnackbar
 import paige.navic.ui.components.dialogs.SideloadingDialog
 import paige.navic.ui.components.sheets.ChangelogSheet
+import paige.navic.ui.components.snackbars.viewmodels.SnackBarViewModel
 import paige.navic.ui.navigation.BottomSheetSceneStrategy
 import paige.navic.ui.navigation.NowPlayingSceneStrategy
 import paige.navic.ui.navigation.Screen
@@ -140,10 +141,10 @@ fun App() {
 		}
 	)
 	val snackbarState = remember { SnackbarHostState() }
-	val player = koinInject<MediaPlayerViewModel>()
+	val snackBarViewModel = koinInject<SnackBarViewModel>()
 
 	LaunchedEffect(Unit) {
-		player.events.collectLatest { event ->
+		snackBarViewModel.events.collectLatest { event ->
 			snackbarState.showSnackbar(getString(event.resource, *event.args.toTypedArray()))
 		}
 	}
