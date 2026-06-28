0b2c90f7fe7b658019d54c19cf5a06d258a7883f
XCroatoanX <stasyukartem2@gmail.com>
2026-06-28 21:06:01 +0200

    remove imports

--- composeApp/src/commonMain/kotlin/paige/navic/domain/manager/SyncManager.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/domain/manager/SyncManager.kt
@@ -3,13 +3,10 @@ package paige.navic.domain.manager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
