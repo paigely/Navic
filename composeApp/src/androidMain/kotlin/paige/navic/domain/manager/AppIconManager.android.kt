9165ae3e86e3c1c9a13f1b155362d98e7583be9b
Heropowwa <fittiziogiovanni@gmail.com>
2026-07-06 14:02:34 +0200

    fix: icons not working properly on some devices

--- composeApp/src/androidMain/kotlin/paige/navic/domain/manager/AppIconManager.android.kt
+++ composeApp/src/androidMain/kotlin/paige/navic/domain/manager/AppIconManager.android.kt
@@ -4,6 +4,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
 import paige.navic.domain.models.settings.AppIconVariant
 
@@ -30,19 +31,29 @@ actual class AppIconManager(
 	}
 
 	actual fun getIcon(variant: AppIconVariant): Any? {
-		val packageManager = context.packageManager
-		val packageName = context.packageName
-		val activityName = "paige.navic.androidApp.${variant.activityName}"
-
-		val drawable = try {
-			val info = packageManager.getActivityInfo(
-				ComponentName(packageName, activityName),
-				PackageManager.GET_META_DATA
-			)
-			info.loadIcon(packageManager)
-		} catch (_: Exception) {
-			packageManager.getApplicationIcon(packageName)
+		val resources = context.resources
+		val iconName = when (variant) {
+			AppIconVariant.Default -> "ic_launcher"
+			AppIconVariant.Inverted -> "ic_launcher_inverted"
 		}
+
+		val iconRes = resources.getIdentifier(iconName, "mipmap", context.packageName)
+		val drawable = if (iconRes != 0) {
+			ResourcesCompat.getDrawable(resources, iconRes, null)
+		} else {
+			val activityName = "paige.navic.androidApp.${variant.activityName}"
+			val componentName = ComponentName(context.packageName, activityName)
+			try {
+				val info = context.packageManager.getActivityInfo(
+					componentName,
+					PackageManager.GET_META_DATA or PackageManager.MATCH_DISABLED_COMPONENTS
+				)
+				val activityResources = context.packageManager.getResourcesForActivity(componentName)
+				ResourcesCompat.getDrawable(activityResources, info.icon, null)
+			} catch (_: Exception) {
+				null
+			}
+		} ?: context.packageManager.getApplicationIcon(context.packageName)
 
 		return drawable.toBitmap(512, 512).asImageBitmap()
 	}
