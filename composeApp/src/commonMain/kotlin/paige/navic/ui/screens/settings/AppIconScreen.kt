9b3164a65b7e01b037c5831ee770bc0f9ac151fe
Heropowwa <fittiziogiovanni@gmail.com>
2026-07-06 14:02:47 +0200

    feat: better icon ui

--- composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/AppIconScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/AppIconScreen.kt
@@ -1,17 +1,13 @@
 package paige.navic.ui.screens.settings
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
@@ -20,27 +16,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import navic.composeapp.generated.resources.Res
-import navic.composeapp.generated.resources.app_name
-import navic.composeapp.generated.resources.title_playback
+import navic.composeapp.generated.resources.option_choose_app_icon
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import paige.navic.domain.manager.AppIconManager
 import paige.navic.domain.manager.PreferenceManager
 import paige.navic.domain.models.settings.AppIconVariant
-import paige.navic.icons.Icons
-import paige.navic.icons.brand.Navic
 import paige.navic.ui.components.common.Form
 import paige.navic.ui.components.common.FormRow
 import paige.navic.ui.components.layouts.NestedTopBar
@@ -50,7 +39,7 @@ fun SettingsAppIconScreen() {
 	val appIconManager = koinInject<AppIconManager>()
 	val preferenceManager = koinInject<PreferenceManager>()
 	Scaffold(
-		topBar = { NestedTopBar({ Text(stringResource(Res.string.title_playback)) }) }
+		topBar = { NestedTopBar({ Text(stringResource(Res.string.option_choose_app_icon)) }) }
 	) { innerPadding ->
 		CompositionLocalProvider(
 			LocalMinimumInteractiveComponentSize provides 0.dp
@@ -59,10 +48,8 @@ fun SettingsAppIconScreen() {
 				Modifier
 					.padding(innerPadding)
 					.verticalScroll(rememberScrollState())
-					.padding(top = 16.dp, end = 16.dp, start = 16.dp)
+					.padding(16.dp)
 			) {
-				AppIconPreview(preferenceManager.appIconVariant)
-
 				Form(Modifier.selectableGroup()) {
 					AppIconVariant.entries.forEach { variant ->
 						FormRow(
@@ -77,6 +64,7 @@ fun SettingsAppIconScreen() {
 								onClick = null
 							)
 							Text(variant.name, Modifier.weight(1f))
+							AppIconItemPreview(variant)
 						}
 					}
 				}
@@ -86,66 +74,24 @@ fun SettingsAppIconScreen() {
 }
 
 @Composable
-fun AppIconPreview(variant: AppIconVariant) {
+fun AppIconItemPreview(variant: AppIconVariant, modifier: Modifier = Modifier) {
 	val appIconManager = koinInject<AppIconManager>()
 	val icon = remember(variant) { appIconManager.getIcon(variant) }
 
-	Box(
-		modifier = Modifier
-			.fillMaxWidth()
-			.padding(bottom = 24.dp)
-			.clip(MaterialTheme.shapes.extraLarge)
-			.background(MaterialTheme.colorScheme.surfaceContainerLow)
-			.padding(vertical = 48.dp),
-		contentAlignment = Alignment.Center
-	) {
-		Column(
-			horizontalAlignment = Alignment.CenterHorizontally,
-			verticalArrangement = Arrangement.spacedBy(8.dp)
-		) {
-			val iconModifier = Modifier
-				.size(84.dp)
-				.clip(MaterialTheme.shapes.large)
+	val iconModifier = modifier
+		.size(48.dp)
+		.clip(MaterialTheme.shapes.medium)
 
-			when (icon) {
-				is ImageBitmap -> Image(
-					bitmap = icon,
-					contentDescription = null,
-					modifier = iconModifier
-				)
-				is Painter -> Image(
-					painter = icon,
-					contentDescription = null,
-					modifier = iconModifier
-				)
-				else -> {
-					val iconBackground =
-						if (variant == AppIconVariant.Inverted) Color.White else Color.Black
-					val iconTint = if (variant == AppIconVariant.Inverted) Color.Black else Color.White
-
-					Box(
-						modifier = iconModifier.background(iconBackground),
-						contentAlignment = Alignment.Center
-					) {
-						Icon(
-							imageVector = Icons.Brand.Navic,
-							contentDescription = null,
-							tint = iconTint,
-							modifier = Modifier.size(56.dp)
-						)
-					}
-				}
-			}
-
-			Text(
-				text = stringResource(Res.string.app_name),
-				style = MaterialTheme.typography.labelLarge.copy(
-					fontWeight = FontWeight.Medium,
-					fontSize = 14.sp,
-					letterSpacing = 0.5.sp
-				),
-				color = MaterialTheme.colorScheme.onSurface
-			)
-		}
+	when (icon) {
+		is ImageBitmap -> Image(
+			bitmap = icon,
+			contentDescription = null,
+			modifier = iconModifier
+		)
+		is Painter -> Image(
+			painter = icon,
+			contentDescription = null,
+			modifier = iconModifier
+		)
 	}
 }
