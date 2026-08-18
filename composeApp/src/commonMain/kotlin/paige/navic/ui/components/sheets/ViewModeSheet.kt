0a6bdd3d7e378bd1d13321b0d6a8986d585bc8e0
heropowwa <fittiziogiovanni@gmail.com>
2026-08-19 00:24:08 +0200

    feat: add icons to viewMode sheet

--- composeApp/src/commonMain/kotlin/paige/navic/ui/components/sheets/ViewModeSheet.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/components/sheets/ViewModeSheet.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
@@ -59,7 +60,13 @@ fun ViewModeSheet(
 						),
 						onClick = { onSetViewMode(viewMode) },
 						selected = selectedViewMode == viewMode,
-						label = { Text(stringResource(viewMode.displayName)) }
+						label = { Text(stringResource(viewMode.displayName)) },
+						icon = {
+							Icon(
+								imageVector = viewMode.icon,
+								contentDescription = null
+							)
+						}
 					)
 				}
 			}
